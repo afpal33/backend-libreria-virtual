6e63604f4ed0f7560d3405bfbbd189606b503b89
fabrizio3443 <andy.palenque@ucb.edu.bo>
2023-11-19 14:58:43 -0600

    cors update

--- src/main/java/ucb/edu/bo/virtlib/backend/config/CorsConfig.java
+++ src/main/java/ucb/edu/bo/virtlib/backend/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/books/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:8082")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
