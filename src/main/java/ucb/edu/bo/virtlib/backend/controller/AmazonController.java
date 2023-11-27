ab6c7fee92f3a83c42c2e35615cc7b27b42f9b03
fabrizio3443 <andy.palenque@ucb.edu.bo>
2023-11-27 17:40:30 -0600

    remake amazon controller

--- src/main/java/ucb/edu/bo/virtlib/backend/controller/AmazonController.java
+++ src/main/java/ucb/edu/bo/virtlib/backend/controller/AmazonController.java
@@ -1,27 +1,38 @@
 package ucb.edu.bo.virtlib.backend.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ucb.edu.bo.virtlib.backend.bl.AmazonApiService;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/api/v1/amazon")
+@RequestMapping("/api/amazon")
 public class AmazonController {
 
-    private final AmazonApiService amazonApiService;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public AmazonController(AmazonApiService amazonApiService) {
-        this.amazonApiService = amazonApiService;
+    public AmazonController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/getPrice/{productName}")
-    public String getProductPrice(@PathVariable String productName) {
-        return amazonApiService.getProductPrice(productName);
+    @GetMapping("/get-prices/{itemName}")
+    public ResponseEntity<String> getPricesFromAmazon(@PathVariable String itemName) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Key", "548851825ac43f460f8ec20f2c8ab823");
+        headers.set("X-RapidAPI-Host", "amazon-data-product-scraper.p.rapidapi.com");
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+                "https://amazon-data-product-scraper.p.rapidapi.com/products/{item}?api_key=548851825ac43f460f8ec20f2c8ab823",
+                String.class,
+                itemName
+        );
+
+        return ResponseEntity.status(responseEntity.getStatusCode())
+                .headers(responseEntity.getHeaders())
+                .body(responseEntity.getBody());
     }
 }
-
-
