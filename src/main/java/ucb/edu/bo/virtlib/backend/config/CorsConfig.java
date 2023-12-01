a5aaa7aef87506bea3aa13692107e52a09d946d2
fabrizio3443 <andy.palenque@ucb.edu.bo>
2023-12-01 14:12:06 -0400

    mod cors

--- src/main/java/ucb/edu/bo/virtlib/backend/config/CorsConfig.java
+++ src/main/java/ucb/edu/bo/virtlib/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("http://localhost:8081", "http://localhost:8082", "http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
