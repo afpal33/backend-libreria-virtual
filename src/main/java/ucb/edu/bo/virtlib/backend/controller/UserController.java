391cff225c46fc7d2688bcf98fda8dc1cc520ccf
fabrizio3443 <andy.palenque@ucb.edu.bo>
2023-12-01 11:47:28 -0400

    last model

--- src/main/java/ucb/edu/bo/virtlib/backend/controller/UserController.java
+++ src/main/java/ucb/edu/bo/virtlib/backend/controller/UserController.java
@@ -1,14 +1,37 @@
 package ucb.edu.bo.virtlib.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ucb.edu.bo.virtlib.backend.bl.UserService;
+import ucb.edu.bo.virtlib.backend.model.UserModel;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/all")
+    public List<UserModel> getAllUsuarios() {
+        return userService.getAllUsuarios();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserModel> getUsuarioById(@PathVariable Long userId) {
+        Optional<UserModel> usuario = userService.getUsuarioById(userId);
+        return usuario.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> borrarUsuarioById(@PathVariable Long userId) {
+        userService.borrarUsuarioById(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
